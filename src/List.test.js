ece127b8cfe2c5350a1e385382e83903616797ff
Willem Green <willberforce15@gmail.com>
2020-09-29 10:52:33 -0500

    first test

--- src/List.test.js
+++ src/List.test.js
@@ -2,3 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './List.js';
 
+it('renders without crashing',  () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<List />, div)
+    ReactDOM.unmountComponentAtNode(div)
+    }
+)
